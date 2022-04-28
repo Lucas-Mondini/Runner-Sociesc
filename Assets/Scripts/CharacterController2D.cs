b0bb235c258f93c579b84e65393783bc04a64d4f
Lucas Fellipe Mondini Pereira <lucasfepereira1@gmail.com>
2022-04-27 21:28:14 -0300

    27-04-2022

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -4,18 +4,35 @@ using UnityEngine.UIElements;
 public class CharacterController2D : MonoBehaviour
 {
     public float AccelerationRight = 0.001f;
+    public float InitialVelocity = 10.0f;
     public float VelocityRight = 0.0f;
     public float MaxVelocity = 1000f;
+    public Rigidbody2D RD;
+    private bool isGrounded = true;
+
     void Start()
     {
-        
+        RD = GetComponent<Rigidbody2D>();
+        RD.velocity = new Vector2(InitialVelocity, 0);
     }
     
     void Update()
     {
-        VelocityRight += AccelerationRight;
-        if (VelocityRight > MaxVelocity)
-            VelocityRight = MaxVelocity;
-        transform.position += new Vector3((transform.position.x + AccelerationRight) * Time.deltaTime, 0, 0);
+        VelocityRight = RD.velocity.x + AccelerationRight;
+        if (VelocityRight <= MaxVelocity)
+        {
+            RD.velocity = new Vector2(VelocityRight, 0);
+        }
+        //var onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            RD.velocity = new Vector2(RD.velocity.x, 5);
+        }
+    }
+
+    void Grounded()
+    {
+        isGrounded = true;
     }
 }
